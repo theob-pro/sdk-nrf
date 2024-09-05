0da8c6235150917f438dd7a81e6048fdd7b082a8
Battrel, Théo <theo.battrel@nordicsemi.no>
2024-09-05 14:02:31 +0200

    Issue reproduced by tech support

---
+++ samples/bluetooth/devzone_330156/central/src/main.c
@@ -0,0 +1,338 @@
+/*
+ * Copyright (c) 2018 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+ */
+
+/** @file
+ *  @brief Nordic UART Service Client sample
+ */
+
+#include <errno.h>
+#include <zephyr/kernel.h>
+#include <zephyr/device.h>
+#include <zephyr/devicetree.h>
+#include <zephyr/sys/byteorder.h>
+#include <zephyr/sys/printk.h>
+
+#include <zephyr/bluetooth/bluetooth.h>
+#include <zephyr/bluetooth/hci.h>
+#include <zephyr/bluetooth/conn.h>
+#include <zephyr/bluetooth/uuid.h>
+#include <zephyr/bluetooth/gatt.h>
+
+#include <bluetooth/services/nus.h>
+#include <bluetooth/services/nus_client.h>
+#include <bluetooth/gatt_dm.h>
+#include <bluetooth/scan.h>
+
+#include <zephyr/settings/settings.h>
+
+#include <zephyr/drivers/uart.h>
+
+#include <zephyr/logging/log.h>
+
+#define LOG_MODULE_NAME central_uart
+LOG_MODULE_REGISTER(LOG_MODULE_NAME);
+
+/* UART payload buffer element size. */
+#define UART_BUF_SIZE 20
+
+struct uart_data_t {
+	void *fifo_reserved;
+	uint8_t  data[UART_BUF_SIZE];
+	uint16_t len;
+};
+
+static K_FIFO_DEFINE(fifo_uart_tx_data);
+static K_FIFO_DEFINE(fifo_uart_rx_data);
+
+static struct bt_conn *default_conn;
+static struct bt_nus_client nus_client;
+
+static void ble_data_sent(struct bt_nus_client *nus, uint8_t err,
+					const uint8_t *const data, uint16_t len)
+{
+	ARG_UNUSED(nus);
+
+	if (err) {
+		LOG_WRN("ATT error code: 0x%02X", err);
+	}
+}
+
+static uint8_t ble_data_received(struct bt_nus_client *nus,
+						const uint8_t *data, uint16_t len)
+{
+	ARG_UNUSED(nus);
+
+	return BT_GATT_ITER_CONTINUE;
+}
+
+
+static void discovery_complete(struct bt_gatt_dm *dm,
+			       void *context)
+{
+	struct bt_nus_client *nus = context;
+	LOG_INF("Service discovery completed");
+
+	bt_gatt_dm_data_print(dm);
+
+	bt_nus_handles_assign(dm, nus);
+	bt_nus_subscribe_receive(nus);
+
+	bt_gatt_dm_data_release(dm);
+}
+
+static void discovery_service_not_found(struct bt_conn *conn,
+					void *context)
+{
+	LOG_INF("Service not found");
+}
+
+static void discovery_error(struct bt_conn *conn,
+			    int err,
+			    void *context)
+{
+	LOG_WRN("Error while discovering GATT database: (%d)", err);
+}
+
+struct bt_gatt_dm_cb discovery_cb = {
+	.completed         = discovery_complete,
+	.service_not_found = discovery_service_not_found,
+	.error_found       = discovery_error,
+};
+
+static void gatt_discover(struct bt_conn *conn)
+{
+	int err;
+
+	if (conn != default_conn) {
+		return;
+	}
+
+	err = bt_gatt_dm_start(conn,
+			       BT_UUID_NUS_SERVICE,
+			       &discovery_cb,
+			       &nus_client);
+	if (err) {
+		LOG_ERR("could not start the discovery procedure, error "
+			"code: %d", err);
+	}
+}
+
+static void exchange_func(struct bt_conn *conn, uint8_t err, struct bt_gatt_exchange_params *params)
+{
+	if (!err) {
+		LOG_INF("MTU exchange done");
+	} else {
+		LOG_WRN("MTU exchange failed (err %" PRIu8 ")", err);
+	}
+}
+
+static void connected(struct bt_conn *conn, uint8_t conn_err)
+{
+	char addr[BT_ADDR_LE_STR_LEN];
+	int err;
+
+	bt_addr_le_to_str(bt_conn_get_dst(conn), addr, sizeof(addr));
+
+	if (conn_err) {
+		LOG_INF("Failed to connect to %s (%d)", addr, conn_err);
+
+		if (default_conn == conn) {
+			bt_conn_unref(default_conn);
+			default_conn = NULL;
+
+			err = bt_scan_start(BT_SCAN_TYPE_SCAN_ACTIVE);
+			if (err) {
+				LOG_ERR("Scanning failed to start (err %d)",
+					err);
+			}
+		}
+
+		return;
+	}
+
+	LOG_INF("Connected: %s", addr);
+
+	static struct bt_gatt_exchange_params exchange_params;
+
+	exchange_params.func = exchange_func;
+	err = bt_gatt_exchange_mtu(conn, &exchange_params);
+	if (err) {
+		LOG_WRN("MTU exchange failed (err %d)", err);
+	}
+
+	gatt_discover(conn);
+
+	err = bt_scan_stop();
+	if ((!err) && (err != -EALREADY)) {
+		LOG_ERR("Stop LE scan failed (err %d)", err);
+	}
+}
+
+static void disconnected(struct bt_conn *conn, uint8_t reason)
+{
+	char addr[BT_ADDR_LE_STR_LEN];
+	int err;
+
+	bt_addr_le_to_str(bt_conn_get_dst(conn), addr, sizeof(addr));
+
+	LOG_INF("Disconnected: %s (reason %u)", addr, reason);
+
+	if (default_conn != conn) {
+		return;
+	}
+
+	bt_conn_unref(default_conn);
+	default_conn = NULL;
+
+	err = bt_scan_start(BT_SCAN_TYPE_SCAN_ACTIVE);
+	if (err) {
+		LOG_ERR("Scanning failed to start (err %d)",
+			err);
+	}
+}
+
+BT_CONN_CB_DEFINE(conn_callbacks) = {
+	.connected = connected,
+	.disconnected = disconnected,
+};
+
+static void scan_filter_match(struct bt_scan_device_info *device_info,
+			      struct bt_scan_filter_match *filter_match,
+			      bool connectable)
+{
+	char addr[BT_ADDR_LE_STR_LEN];
+
+	bt_addr_le_to_str(device_info->recv_info->addr, addr, sizeof(addr));
+
+	LOG_INF("Filters matched. Address: %s connectable: %d",
+		addr, connectable);
+}
+
+static void scan_connecting_error(struct bt_scan_device_info *device_info)
+{
+	LOG_WRN("Connecting failed");
+}
+
+static void scan_connecting(struct bt_scan_device_info *device_info,
+			    struct bt_conn *conn)
+{
+	default_conn = bt_conn_ref(conn);
+}
+
+static int nus_client_init(void)
+{
+	int err;
+	struct bt_nus_client_init_param init = {
+		.cb = {
+			.received = ble_data_received,
+			.sent = ble_data_sent,
+		}
+	};
+
+	err = bt_nus_client_init(&nus_client, &init);
+	if (err) {
+		LOG_ERR("NUS Client initialization failed (err %d)", err);
+		return err;
+	}
+
+	LOG_INF("NUS Client module initialized");
+	return err;
+}
+
+BT_SCAN_CB_INIT(scan_cb, scan_filter_match, NULL,
+		scan_connecting_error, scan_connecting);
+
+static int scan_init(void)
+{
+	int err;
+	struct bt_scan_init_param scan_init = {
+		.connect_if_match = 1,
+	};
+
+	bt_scan_init(&scan_init);
+	bt_scan_cb_register(&scan_cb);
+
+	err = bt_scan_filter_add(BT_SCAN_FILTER_TYPE_UUID, BT_UUID_NUS_SERVICE);
+	if (err) {
+		LOG_ERR("Scanning filters cannot be set (err %d)", err);
+		return err;
+	}
+
+	err = bt_scan_filter_enable(BT_SCAN_UUID_FILTER, false);
+	if (err) {
+		LOG_ERR("Filters cannot be turned on (err %d)", err);
+		return err;
+	}
+
+	LOG_INF("Scan module initialized");
+	return err;
+}
+
+void command_sent(struct bt_conn *conn, void *user_data)
+{
+	ARG_UNUSED(user_data);
+
+	__ASM("nop");
+}
+
+int main(void)
+{
+	int err;
+	uint32_t pkt_cnt = 0;
+	uint8_t data[244];
+
+	memset(data, 0xAA, sizeof(data));
+
+	err = bt_enable(NULL);
+	if (err) {
+		LOG_ERR("Bluetooth init failed (err %d)", err);
+		return 0;
+	}
+	LOG_INF("Bluetooth initialized");
+
+	if (IS_ENABLED(CONFIG_SETTINGS)) {
+		settings_load();
+	}
+
+	err = scan_init();
+	if (err != 0) {
+		LOG_ERR("scan_init failed (err %d)", err);
+		return 0;
+	}
+
+	err = nus_client_init();
+	if (err != 0) {
+		LOG_ERR("nus_client_init failed (err %d)", err);
+		return 0;
+	}
+
+	printk("Starting Bluetooth Central UART example\n");
+
+	err = bt_scan_start(BT_SCAN_TYPE_SCAN_ACTIVE);
+	if (err) {
+		LOG_ERR("Scanning failed to start (err %d)", err);
+		return 0;
+	}
+
+	LOG_INF("Scanning successfully started");
+
+	for (;;) {
+		if (nus_client.conn) {
+			err = bt_gatt_write_without_response_cb(nus_client.conn,
+				nus_client.handles.rx, data, sizeof(data), false, command_sent, NULL);
+			if (!err)
+			{
+				pkt_cnt++;
+			}
+		}
+
+		if(pkt_cnt % 100 == 0) {
+			LOG_INF("100 notifications sent.");
+		}
+
+		k_msleep(1);
+	}
+}
---
+++ samples/bluetooth/devzone_330156/peripheral/src/main.c
@@ -0,0 +1,170 @@
+/*
+ * Copyright (c) 2018 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+ */
+
+/** @file
+ *  @brief Nordic UART Bridge Service (NUS) sample
+ */
+#include "uart_async_adapter.h"
+
+#include <zephyr/types.h>
+#include <zephyr/kernel.h>
+#include <zephyr/drivers/uart.h>
+#include <zephyr/usb/usb_device.h>
+
+#include <zephyr/device.h>
+#include <zephyr/devicetree.h>
+#include <soc.h>
+
+#include <zephyr/bluetooth/bluetooth.h>
+#include <zephyr/bluetooth/uuid.h>
+#include <zephyr/bluetooth/gatt.h>
+#include <zephyr/bluetooth/hci.h>
+
+#include <bluetooth/services/nus.h>
+
+#include <zephyr/settings/settings.h>
+
+#include <stdio.h>
+
+#include <zephyr/logging/log.h>
+
+#define LOG_MODULE_NAME peripheral_uart
+LOG_MODULE_REGISTER(LOG_MODULE_NAME);
+
+#define STACKSIZE CONFIG_BT_NUS_THREAD_STACK_SIZE
+#define PRIORITY 7
+
+#define DEVICE_NAME CONFIG_BT_DEVICE_NAME
+#define DEVICE_NAME_LEN	(sizeof(DEVICE_NAME) - 1)
+
+#define UART_BUF_SIZE CONFIG_BT_NUS_UART_BUFFER_SIZE
+#define UART_WAIT_FOR_BUF_DELAY K_MSEC(50)
+#define UART_WAIT_FOR_RX CONFIG_BT_NUS_UART_RX_WAIT_TIME
+
+static K_SEM_DEFINE(ble_init_ok, 0, 1);
+
+static struct bt_conn *current_conn;
+
+static const struct bt_data ad[] = {
+	BT_DATA_BYTES(BT_DATA_FLAGS, (BT_LE_AD_GENERAL | BT_LE_AD_NO_BREDR)),
+	BT_DATA(BT_DATA_NAME_COMPLETE, DEVICE_NAME, DEVICE_NAME_LEN),
+};
+
+static const struct bt_data sd[] = {
+	BT_DATA_BYTES(BT_DATA_UUID128_ALL, BT_UUID_NUS_VAL),
+};
+
+static void connected(struct bt_conn *conn, uint8_t err)
+{
+	char addr[BT_ADDR_LE_STR_LEN];
+
+	if (err) {
+		LOG_ERR("Connection failed (err %u)", err);
+		return;
+	}
+
+	bt_addr_le_to_str(bt_conn_get_dst(conn), addr, sizeof(addr));
+	LOG_INF("Connected %s", addr);
+
+	current_conn = bt_conn_ref(conn);
+}
+
+static void disconnected(struct bt_conn *conn, uint8_t reason)
+{
+	char addr[BT_ADDR_LE_STR_LEN];
+
+	bt_addr_le_to_str(bt_conn_get_dst(conn), addr, sizeof(addr));
+
+	LOG_INF("Disconnected: %s (reason %u)", addr, reason);
+
+	if (current_conn) {
+		bt_conn_unref(current_conn);
+		current_conn = NULL;
+	}
+}
+
+BT_CONN_CB_DEFINE(conn_callbacks) = {
+	.connected    = connected,
+	.disconnected = disconnected,
+};
+
+static void bt_receive_cb(struct bt_conn *conn, const uint8_t *const data,
+			  uint16_t len)
+{
+	static uint32_t packets_received;
+
+	packets_received++;
+
+	if (packets_received % 100 == 0) {
+		LOG_INF("100 packets received");
+	}
+
+}
+
+static struct bt_nus_cb nus_cb = {
+	.received = bt_receive_cb,
+};
+
+int main(void)
+{
+	int err = 0;
+
+	err = bt_enable(NULL);
+	if (err) {
+		LOG_ERR("Failed to initialize Bluetooth (err: %d)", err);
+		return 0;
+	}
+
+	LOG_INF("Bluetooth initialized");
+
+	k_sem_give(&ble_init_ok);
+
+	if (IS_ENABLED(CONFIG_SETTINGS)) {
+		settings_load();
+	}
+
+	err = bt_nus_init(&nus_cb);
+	if (err) {
+		LOG_ERR("Failed to initialize UART service (err: %d)", err);
+		return 0;
+	}
+
+	err = bt_le_adv_start(BT_LE_ADV_CONN, ad, ARRAY_SIZE(ad), sd,
+			      ARRAY_SIZE(sd));
+	if (err) {
+		LOG_ERR("Advertising failed to start (err %d)", err);
+		return 0;
+	}
+
+	for (;;) {
+		k_sleep(K_MSEC(1000));
+	}
+}
+
+#include <zephyr/random/random.h>
+
+void ble_write_thread(void)
+{
+	/* Don't go any further until BLE is initialized */
+	k_sem_take(&ble_init_ok, K_FOREVER);
+
+	uint32_t random_num;
+
+	sys_csrand_get(&random_num, sizeof(random_num));
+
+    // Scale the number to the range 10 to 1000
+    uint32_t scaled_random_num = 10 + (random_num % (1000 - 10 + 1));
+	uint32_t random_delay_ms = scaled_random_num * 5;
+
+	LOG_INF("Delay before reset: %d ms", random_delay_ms);
+	for (;;) {
+		k_msleep(random_delay_ms);
+		NVIC_SystemReset();
+	}
+}
+
+K_THREAD_DEFINE(ble_write_thread_id, STACKSIZE, ble_write_thread, NULL, NULL,
+		NULL, PRIORITY, 0, 0);
